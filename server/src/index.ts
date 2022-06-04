3aca17c79781b3a5c962b760b3dfe40014a9c30d
Andrew-CC-Martin <amartin262@gmail.com>
2022-06-04 15:24:34 +1000

    add cookieparser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+
 const allowedOrigins = [
   "http://localhost:3000",
   "https://warm-frangollo-0dc699.netlify.app/",
 ];
-
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
 app.use(cors(options));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 const port = process.env.PORT || 4000;
 
 app.get("/", (_req, res) => {
@@ -17,7 +20,7 @@ app.get("/", (_req, res) => {
 });
 
 app.get("/hello", (_req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello again, world!");
 });
 
 // start the Express server
