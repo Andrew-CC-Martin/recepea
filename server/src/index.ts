c122ea3c563e2a435c0114980551cf8e75fb0391
Andrew-CC-Martin <amartin262@gmail.com>
2022-06-04 14:38:03 +1000

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
-// const express = require("express");
 import express from "express";
+import cors from "cors";
 const app = express();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://warm-frangollo-0dc699.netlify.app/",
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(options));
 const port = 4000; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello world!");
 });
 
