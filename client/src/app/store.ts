b11c8db688d42d6ef17a12acbf5e910cb7cde57f
Andrew-CC-Martin <amartin262@gmail.com>
2022-06-13 22:46:47 +1000

    bugfix - remove serializableCheck from redux store (to support redux-persist)

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -16,6 +16,10 @@ export const store = configureStore({
     counter: counterReducer,
     auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
